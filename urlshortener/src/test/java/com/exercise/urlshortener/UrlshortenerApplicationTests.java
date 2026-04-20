edccc7b0914a866f4059d937015a6305dbf74d23
Mahir Aydinoglu <mhrydngl@gmail.com>
2026-04-20 15:12:31 +0100

    initial spring boot setup

---
+++ urlshortener/src/test/java/com/exercise/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
