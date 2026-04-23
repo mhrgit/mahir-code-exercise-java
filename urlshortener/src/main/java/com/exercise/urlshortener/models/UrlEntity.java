2a7ce4f66c1ad817c3980e18a8052a236f194bfb
Mahir Aydinoglu <mhrydngl@gmail.com>
2026-04-23 14:29:05 +0100

    models added and db connection details added

--- urlshortener/src/main/java/com/exercise/urlshortener/models/UrlEntity.java
+++ urlshortener/src/main/java/com/exercise/urlshortener/models/UrlEntity.java
@@ -1,5 +1,58 @@
 package com.exercise.urlshortener.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity(name = "url")
+@Table(name = "urls")
 public class UrlEntity {
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+    private Long id;
+
+    @Column(name = "longurl")
+    private String longUrl;
+
+    @Column(name = "shorturl")
+    private String shortUrl;
+
+    public UrlEntity() {
+    }
+
+    public UrlEntity(Long id, String longUrl, String shortUrl) {
+        this.id = id;
+        this.longUrl = longUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullUrl() {
+        return longUrl;
+    }
+
+    public void setFullUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
 
 }
